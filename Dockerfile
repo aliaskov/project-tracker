ea61bc9d1c5c12a8df34b08646ddb4b4ccb3b170
edward.kats.course <edward.kats.course@gmail.com>
2018-11-16 19:01:43 +0200

    1. Added docker-compose 2. modified application to work with h2 and mysql separately 3. modified changelog 4. modified dockerfile 5. added project-tracker delay by env variable

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:8
 ADD target/project-tracker.jar project-tracker.jar
 EXPOSE 8080
-ENTRYPOINT ["java", "-jar", "project-tracker.jar"]
+ENTRYPOINT ["java", "-Dspring.profiles.active=docker", "-jar", "project-tracker.jar"]
--- src/main/java/com/telran/project/tracker/ProjectTrackerServerApplication.java
+++ src/main/java/com/telran/project/tracker/ProjectTrackerServerApplication.java
@@ -1,12 +1,38 @@
 package com.telran.project.tracker;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Optional;
+
+@Slf4j
 @SpringBootApplication
 public class ProjectTrackerServerApplication {
 
-    public static void main(String[] args) {
+    private static final String DELAY_ENV_VARIABLE_NAME = "START_DELAY";
+    private static final Long DELAY_DEFAULT = 500L;
+
+    public static void main(String[] args) throws InterruptedException {
+        log.info("Starting Project-Tracker application");
+
+        log.info("Delay value is {}present in env variable START_DELAY", System.getenv(DELAY_ENV_VARIABLE_NAME) == null ? "NOT " : "");
+
+        Long delay = Optional
+                .ofNullable(System.getenv(DELAY_ENV_VARIABLE_NAME))
+                .map(x -> {
+                    try {
+                        Long.valueOf(x);
+                    } catch (NumberFormatException e) {
+                        log.error("START_DELAY env variable must be digit");
+                        throw new RuntimeException("START_DELAY env variable must be digit", e);
+                    }
+                    return Long.valueOf(x);
+                })
+                .orElse(DELAY_DEFAULT);
+        log.info("Delay is {}", delay);
+
+        Thread.sleep(delay);
         SpringApplication.run(ProjectTrackerServerApplication.class, args);
     }
 }
